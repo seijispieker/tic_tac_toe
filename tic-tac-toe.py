770539ea59568e7a048dda922e4173d0f869fde3
Seiji Spieker <seiji.spieker@gmail.com>
2022-05-25 11:59:04 +0200

    is_winner method

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,4 @@
-class Board:
+class Game:
     players = {'X', 'O'}
 
     def __init__(self):
@@ -12,3 +12,27 @@ class Board:
             raise Exception('Invalid Move.')
 
         self.matrix[x][y] = player
+
+    def is_winner(self):
+        return (self.check_rows() or self.check_columns()
+                or self.check_diagonals())
+
+    def check_rows(self):
+        for row in self.matrix:
+            if row[0] and all(entry == row[0] for entry in row):
+                return row[0]
+
+    def check_columns(self):
+        for j in range(3):
+            if self.matrix[0][j] and all(self.matrix[i][j] == self.matrix[0][j]
+                                         for i in range(3)):
+                return self.matrix[0][j]
+
+    def check_diagonals(self):
+        if self.matrix[0][0] and all(self.matrix[i][i] == self.matrix[0][0]
+                                     for i in range(3)):
+            return self.matrix[0]
+
+        if self.matrix[0][2] and all(self.matrix[i][3 - i] == self.matrix[2][2]
+                                     for i in range(3)):
+            return self.matrix[2][2]
