7b103663fa7757153caaded14ad19ac543cdfb3d
Seiji Spieker <seiji.spieker@gmail.com>
2022-07-02 11:36:13 +0200

    Ask indices in one line.

--- tic_tac_toe/__main__.py
+++ tic_tac_toe/__main__.py
@@ -1,5 +1,5 @@
 """Main script that starts the tic-tac-toe game."""
-from typing import Tuple
+from typing import List, Tuple
 
 from tic_tac_toe import tic_tac_toe
 
@@ -35,28 +35,15 @@ def main() -> None:
 def ask_indices() -> Tuple[int, int]:
     """Ask the user for row and column indices and return as tuple."""
     while True:
-        i_string = input('Row index: ')
+        user_input = input("Indices '<row index>, <column index>': ")
+        # Split user input by comma and strip leading and trailing characters.
+        indices_string: List[str] = list(map(str.strip, user_input.split(',')))
 
-        if i_string.isnumeric():
-            i = int(i_string)
-
-            if i in tic_tac_toe.POSSIBLE_INDICES:
-                break
-
-        print('Invalid row index.')
-
-    while True:
-        j_string = input('Column index: ')
-
-        if j_string.isnumeric():
-            j = int(j_string)
-
-            if j in tic_tac_toe.POSSIBLE_INDICES:
-                break
-
-        print('Invalid column index.')
-
-    return i, j
+        if (len(indices_string) is 2 and indices_string[0].isnumeric()
+                and indices_string[1].isnumeric()):
+            return int(indices_string[0]), int(indices_string[1])
+        else:
+            print('Invalid indices.')
 
 
 if __name__ == '__main__':
