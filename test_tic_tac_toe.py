8a47e6e0616b43e9a2a89f503b17ce36ec03c9da
Seiji Spieker <seiji.spieker@gmail.com>
2022-07-30 10:39:54 +0200

    Tests __init__ of TicTacToe.

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,21 +1,56 @@
 """Tests functionality of the tic_tac_toe.tic_tac_toe module."""
+import itertools
+import random
+from typing import List
 import unittest
 
 from tic_tac_toe import tic_tac_toe
 
 
-class InitTicTacToeTestCase(unittest.TestCase):
+class TicTacToeInitTestCase(unittest.TestCase):
     """Tests __init__ method of TicTacToe."""
 
-    def test_tic_tac_toe_init_without_argument(self) -> None:
-        """Tests if _matrix attribute is a deep copy of INITIAL_MATRIX."""
+    def test_tic_tac_toe_init_deep_copy_initial_matrix(self) -> None:
+        """Tests __init__ method of TicTacToe without arguments.
+
+        Asserts if _matrix attribute is a deep copy of INITIAL_MATRIX, the
+        default value of the paramater matrix.
+        """
         tic_tac_toe_object = tic_tac_toe.TicTacToe()
-        self.assertListEqual(tic_tac_toe_object._matrix,
-                             tic_tac_toe.INITIAL_MATRIX,
-                             '_matrix is not equal to INITIAL_MATRIX')
-        self.assertIsNot(tic_tac_toe_object._matrix,
-                         tic_tac_toe.INITIAL_MATRIX,
-                         '_matrix is not a deep copy of INITIAL_MATRIX')
+        self.assert_matrix_deep_copy(copy=tic_tac_toe_object._matrix,
+                                     original=tic_tac_toe.INITIAL_MATRIX)
+
+    def test_tic_tac_toe_init_with_argument(self, k: int = 10**10) -> None:
+        """Tests __init__ method of TicTacToe with arguments.
+
+        Asserts if _matrix attribute is a deep copy of random sample of size
+        k of all the possible 3 by 3 matrices.
+        """
+        rows = itertools.product((player for player in tic_tac_toe.Player),
+                                 repeat=tic_tac_toe.SIZE)
+        matrices = itertools.product(rows, repeat=tic_tac_toe.SIZE)
+
+        if k > (n := len(tic_tac_toe.Player)**(tic_tac_toe.SIZE**2)):
+            k = n
+
+        sample = random.sample(list(matrices), k=k)
+
+        for matrix in sample:
+            with self.subTest(matrix=matrix):
+                matrix_list = [list(row) for row in matrix]
+                tic_tac_toe_object = tic_tac_toe.TicTacToe(matrix_list)
+                self.assert_matrix_deep_copy(copy=tic_tac_toe_object._matrix,
+                                             original=matrix_list)
+
+    def assert_matrix_deep_copy(self, copy: List[List[tic_tac_toe.Player]],
+                                original: List[List[tic_tac_toe.Player]]) -> None:
+        """Asserts if copy matrix is an deep copy of original matrix."""
+        self.assertListEqual(copy, original, '_matrix is not a copy')
+        self.assertIsNot(copy, original, '_matrix is not a deep copy')
+
+        for _matrix_row, original_row in zip(copy, original):
+            self.assertIsNot(_matrix_row, original_row,
+                             'row of _matrix is not a deep copy')
 
 
 if __name__ == '__main__':
